7e56d73c5eb8370ef61291f21003a0d9054f826a
救死扶伤韩天尊 <458875122@qq.com>
2026-08-15 22:39:36 +0800

    Update 1 file: initTermux.sh

--- bash_script/initTermux.sh
+++ bash_script/initTermux.sh
@@ -6,17 +6,21 @@ readonly setPS1="$SCRIPT_DIR/setPS1.sh"
 readonly setTermuxProp="$SCRIPT_DIR/setTermuxProp.sh"
 readonly changeTermuxRepo="$SCRIPT_DIR/changeTermuxRepo.sh"
 readonly setNvimConfig="$SCRIPT_DIR/setNvimConfig.sh"
+readonly setAndroidSdk="$SCRIPT_DIR/setAndroidSdk.sh""
 ########################################
 main() {
     echo "欢迎使用初始化脚本"
-    ehco "正在换源中..."
+    echo "正在换源中..."
     bash "$changeTermuxRepo"
-    ehco "设置PS1中..."
+    echo "设置PS1中..."
     bash "$setPS1"
-    ehco "设置termux.prop中..."
+    echo "设置termux.prop中..."
     bash "$setTermuxProp"
-    ehco "设置nvim配置中..."
+    echo "设置nvim配置中..."
     bash "$setNvimConfig"
+    echo "设置AndroidSdk中..."
+    bash "$setAndroidSdk"
+    
 }
 ########################################
 
